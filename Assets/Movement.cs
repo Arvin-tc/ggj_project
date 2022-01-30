9add8976dda5f4d3bd241e4089de427cf0be09e6
caisitan <73937249+caisitan@users.noreply.github.com>
2022-01-29 16:59:41 -0800

    Player_past and wall complete

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject Player_past;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Queue<Vector2> record = new Queue<Vector2>();
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,36 +20,54 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //current movement
         float vertical = 0;
         float horizontal = 0;
         if (Input.GetKeyDown(KeyCode.A))
         {
             horizontal = -1;
             moveInput = new Vector2(horizontal, vertical);
+            record.Enqueue(moveInput);
             Move();
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             horizontal = 1;
             moveInput = new Vector2(horizontal, vertical);
+            record.Enqueue(moveInput);
             Move();
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             vertical = 1;
             moveInput = new Vector2(horizontal, vertical);
+            record.Enqueue(moveInput);
             Move();
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             vertical = -1;
             moveInput = new Vector2(horizontal, vertical);
+            record.Enqueue(moveInput);
             Move();
         }
-        
+        //start player_past movement
+        past_move();
     }
     private void Move()
     {
         rb.MovePosition(rb.position + moveInput);
+    }
+    //player_past movement
+    private void past_move()
+    {
+        if (record.Count < 4)
+        {
+            return;
+        }
+        else
+        {
+            Player_past.GetComponent<Rigidbody2D>().MovePosition(Player_past.GetComponent<Rigidbody2D>().position + record.Dequeue());
+        }
     }
 }
---
+++ Assets/PlayerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private Vector2 moveInput;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Box")
+        {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            Debug.Log(Helper());
+            rb.MovePosition(rb.position + moveInput);
+        }
+    }
+    private Vector2 Helper()
+    {
+        float vertical = 0;
+        float horizontal = 0;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            horizontal = -1;
+            return moveInput = new Vector2(horizontal, vertical);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            horizontal = 1;
+            return moveInput = new Vector2(horizontal, vertical);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            vertical = 1;
+            return moveInput = new Vector2(horizontal, vertical);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            vertical = -1;
+            return moveInput = new Vector2(horizontal, vertical);
+        }
+        return moveInput;
+    }
+}
