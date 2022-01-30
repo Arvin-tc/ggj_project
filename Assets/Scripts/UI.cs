9ab112877ee90e699f1c208c28ae99e2b83f63ab
Tiancl10 <57109344+Arvin-tc@users.noreply.github.com>
2022-01-29 16:49:58 -0800

    UI update

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void SelectLevel() 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void Restart() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+}
